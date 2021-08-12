3fd39e08637e02158c2be05508d3aeb219a15b68
Yoichi Kobayashi <info@tplh.net>
2021-08-12 21:41:54 +0900

    update eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,30 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    'prettier',
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'quotes': ['error', 'single'],
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'never',
+    }],
+    'semi': ['error', 'never', {'beforeStatementContinuationChars': 'never'}],
+    'semi-spacing': ['error', {'after': true, 'before': false}],
+    'semi-style': ['error', 'first'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+  },
 }
