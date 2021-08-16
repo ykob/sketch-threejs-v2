4c2882fbc8b8a49dc14210b65f1e8aa053e8583f
Yoichi Kobayashi <info@tplh.net>
2021-08-17 02:52:06 +0900

    import wegl plugin in nuxt.config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/utils',
+    '@/plugins/webgl',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
