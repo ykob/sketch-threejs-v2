0398eb26a43374090646f826a950e745a5f9dc07
Yoichi Kobayashi <info@tplh.net>
2021-08-17 02:51:21 +0900

    added global declaring type file.

---
+++ @types/global.d.ts
@@ -0,0 +1,10 @@
+declare module '*.css';
+declare module '*.scss';
+declare module '*.vs' {
+  const src: string
+  export default src
+}
+declare module '*.fs' {
+  const src: string
+  export default src
+}
