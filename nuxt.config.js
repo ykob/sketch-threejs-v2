0b0352595c6de07e34843f3170d7ac08d151fbb9
Yoichi Kobayashi <info@tplh.net>
2021-09-10 22:23:05 +0900

    updated viewport property.

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     title: 'sketch-threejs-v2',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
