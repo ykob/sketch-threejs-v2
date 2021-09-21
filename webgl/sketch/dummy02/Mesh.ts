18a7527ca577e9675f5ba467f940f71567550b87
Yoichi Kobayashi <info@tplh.net>
2021-09-21 23:51:23 +0900

    update dummy02 mesh geometry.

--- webgl/sketch/dummy02/Mesh.ts
+++ webgl/sketch/dummy02/Mesh.ts
@@ -2,15 +2,15 @@ import * as THREE from 'three'
 
 export default class Mesh extends THREE.Mesh {
   constructor() {
-    const geometry = new THREE.TorusGeometry(35, 4, 16, 100)
+    const geometry = new THREE.TorusKnotGeometry(20, 4, 100, 32)
     const material = new THREE.MeshBasicMaterial({
-      color: new THREE.Color(0x00ff00),
+      color: new THREE.Color(0x0000ff),
     })
     super(geometry, material)
   }
 
   update(time: number) {
-    this.rotation.x += time * 0.5
-    this.rotation.y -= time
+    this.rotation.x += time
+    this.rotation.y += time
   }
 }
