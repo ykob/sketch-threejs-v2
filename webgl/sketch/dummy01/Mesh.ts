f7d4e99c7dbcf94a0d6752815555f4a5303ad387
Yoichi Kobayashi <info@tplh.net>
2021-09-21 23:54:00 +0900

    update dummy01 mesh.

--- webgl/sketch/dummy01/Mesh.ts
+++ webgl/sketch/dummy01/Mesh.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 
 export default class Mesh extends THREE.Mesh {
   constructor() {
-    const geometry = new THREE.TorusGeometry(35, 4, 16, 100)
+    const geometry = new THREE.TorusGeometry(30, 5, 16, 100)
     const material = new THREE.MeshBasicMaterial({
       color: new THREE.Color(0x00ff00),
     })
