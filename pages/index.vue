9b71fd5f2c34b2f8f72f9ec22eb86a0b3dffd976
Yoichi Kobayashi <info@tplh.net>
2021-09-28 02:41:27 +0900

    update the content json structure.

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,32 @@
 <template lang="pug">
 div
-  .navi
-    .navi__item(
-      @click = 'changeSketch1'
-      )
-      |home
-    .navi__item(
-      @click = 'changeSketch2'
-      )
-      |dummy01
-    .navi__item(
-      @click = 'changeSketch3'
-      )
-      |dummy02
-    .navi__item(
-      @click = 'changeSketch4'
-      )
-      |dummy03
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+// import { IContentDocument } from '@nuxt/content/types/content'
+import { Page } from '@/@types/frontend'
 
 export default Vue.extend({
   async asyncData({ $content }) {
-    const response = await $content('pages').fetch()
+    const response = await $content('home').fetch()
+
     return {
       page: response,
     }
   },
   data: (): {
-    page: any
+    page: Page
   } => ({
-    page: null,
+    page: {
+      title: '',
+      webgl: '',
+    },
   }),
   created() {
-    if (this.page === null) return 
-    this.$webgl.changeSketch(this.page.dummy01.webgl)
-  },
-  methods: {
-    changeSketch1() {
-      if (this.page === null) return 
-      this.$webgl.changeSketch(this.page.home.webgl)
-    },
-    changeSketch2() {
-      if (this.page === null) return 
-      this.$webgl.changeSketch(this.page.dummy01.webgl)
-    },
-    changeSketch3() {
-      if (this.page === null) return 
-      this.$webgl.changeSketch(this.page.dummy02.webgl)
-    },
-    changeSketch4() {
-      if (this.page === null) return 
-      this.$webgl.changeSketch(this.page.dummy03.webgl)
-    },
+    this.$webgl.changeSketch(this.page.webgl)
   },
 })
 </script>
 
-<style lang="scss" scoped>
-.navi {
-  color: white;
-  position: absolute;
-  z-index: 2;
-  bottom: 20px;
-  right: 20px;
-  &__item {
-    cursor: pointer;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
