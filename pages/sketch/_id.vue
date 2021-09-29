f67b2e08e96c80da05c577285eb4b6a88f141e7d
Yoichi Kobayashi <info@tplh.net>
2021-09-29 16:02:25 +0900

    added the sketch page component.

---
+++ pages/sketch/_id.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+div
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Page } from '@/@types/frontend'
+
+export default Vue.extend({
+  async asyncData({ params, $content }) {
+    const response = await $content(params.id).fetch()
+
+    return {
+      page: response,
+    }
+  },
+  data: (): {
+    page: Page
+  } => ({
+    page: {
+      title: '',
+      webgl: '',
+    },
+  }),
+  created() {
+    this.$webgl.changeSketch(this.page.webgl)
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
