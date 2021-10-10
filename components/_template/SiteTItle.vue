233879097e4fb77095a9a36aa07393180e5e8516
Yoichi Kobayashi <info@tplh.net>
2021-10-11 02:22:32 +0900

    added site title template component.

---
+++ components/_template/SiteTItle.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.site-title
+  nuxt-link(
+    to = '/'
+    )
+    |sketch of three.js v2
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.site-title {
+  position: absolute;
+  top: 24px;
+  left: 24px;
+  z-index: z(console);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
 div
+  SiteTItle
   Nuxt
   canvas#canvas-webgl
   Navi
