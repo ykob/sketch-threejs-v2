c79f29546da735d6806523bcc08d7e93af6978d5
Yoichi Kobayashi <info@tplh.net>
2021-10-20 13:10:57 +0900

    added transition effect to LinkUnderline component.

--- components/_common/LinkUnderline.vue
+++ components/_common/LinkUnderline.vue
@@ -2,20 +2,44 @@
 nuxt-link.link-underline(
   :to = 'to'
   )
-  slot
-  .link-underline__line
-  .link-underline__line
-  .link-underline__line
+  span(
+    @mouseover = 'mouseOver'
+    @mouseleave = 'mouseLeave'
+    )
+    slot
+  .link-underline__line(
+    v-for = 'bool, index in lines'
+    )
+    transition
+      .link-underline__line-in(
+        v-show = 'current === index && isOvered === true'
+        )
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
+const MAX = 3
 
 export default Vue.extend({
   props: {
     to: {
       type: String,
       default: '',
+    },
+  },
+  data: () => ({
+    current: -1,
+    isOvered: false,
+    lines: Array(MAX),
+  }),
+  methods: {
+    mouseOver() {
+      this.isOvered = true
+    },
+    mouseLeave() {
+      this.isOvered = false
+      this.current = (this.current + 1) % MAX
     },
   },
 })
@@ -28,8 +52,31 @@ export default Vue.extend({
   &__line {
     width: 100%;
     height: 1px;
+    display: block;
+    overflow: hidden;
     position: absolute;
+    top: 50%;
+    left: 0;
+    pointer-events: none;
+  }
+  &__line-in {
+    width: 100%;
+    height: 100%;
     background-color: currentColor;
+    &.v-enter {
+      transform: translate3d(-101%, 0, 0);
+    }
+    &.v-enter-to {
+      transform: translate3d(0, 0, 0);
+      transition-duration: 0.4s;
+    }
+    &.v-leave {
+      transform: translate3d(0, 0, 0);
+    }
+    &.v-leave-to {
+      transform: translate3d(101%, 0, 0);
+      transition-duration: 0.4s;
+    }
   }
 }
 </style>
