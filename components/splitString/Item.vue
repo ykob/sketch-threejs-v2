9ffdd5e7503cd6382df2ace2f1ae55807b013607
Yoichi Kobayashi <info@tplh.net>
2021-10-26 13:47:24 +0900

    added rondom number to rotate transition on Sketch TItle typos.

--- components/splitString/Item.vue
+++ components/splitString/Item.vue
@@ -2,6 +2,7 @@
 transition
   .split-string-item(
     :style = 'styles'
+    :class = 'classnames'
     )
     slot
 </template>
@@ -25,6 +26,12 @@ export default Vue.extend({
         transitionDelay: `${this.delay}s`,
       }
     },
+    classnames(): string[] {
+      return [
+        `rotate-enter-${Math.ceil(Math.random() * 7)}`,
+        `rotate-leave-${Math.ceil(Math.random() * 7)}`,
+      ]
+    },
   },
   mounted() {
     this.delay = Math.random() * this.delayMax
@@ -35,9 +42,15 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .split-string-item {
   display: inline-block;
+  transition-property: none;
   .v-enter & {
     opacity: 0;
-    transform: translate3d(0, 0.5em, 0) rotate3d(0.4, 1, 0, 45deg);
+    @for $i from 1 through 7 {
+      &.rotate-enter-#{$i} {
+        $x: math.div($i, 7) * 1.6 - 0.8;
+        transform: translate3d(0, 0.3em, 0) rotate3d(#{$x}, 1, 0, 45deg);
+      }
+    }
   }
   .v-enter-to &,
   .page-leave-to & {
@@ -54,8 +67,13 @@ export default Vue.extend({
   }
   .page-leave-to & {
     opacity: 0;
-    transform: translate3d(0, -0.5em, 0) rotate3d(0.4, 1, 0, -45deg);
     transition-timing-function: $easeInCubic;
+    @for $i from 1 through 7 {
+      &.rotate-leave-#{$i} {
+        $x: math.div($i, 7) * 1.6 - 0.8;
+        transform: translate3d(0, -0.7em, 0) rotate3d(#{$x}, 1, 0, -75deg);
+      }
+    }
   }
 }
 </style>
