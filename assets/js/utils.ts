3805197e09df25cda879fd6835ca8fadf3b7e7da
Yoichi Kobayashi <info@tplh.net>
2021-11-04 00:48:09 +0900

    added MathEx object.

--- assets/js/utils.ts
+++ assets/js/utils.ts
@@ -4,6 +4,43 @@ const sleep = (delay: number): Promise<void> => {
   })
 }
 
+const MathEx = {
+  degrees: (radian: number): number => {
+    return radian / Math.PI * 180
+  },
+  radians: (degree: number): number => {
+    return degree * Math.PI / 180
+  },
+  clamp: (value: number, min: number, max: number): number => {
+    return Math.min(Math.max(value, min), max)
+  },
+  mix: (x1: number, x2: number, a: number): number => {
+    return x1 * (1 - a) + x2 * a
+  },
+  step: (e: number, x: number): number => {
+    return (x >= e) ? 1 : 0
+  },
+  smoothstep: (e0: number, e1: number, x: number): number | undefined => {
+    if (e0 >= e1) return undefined
+    const t = Math.min(Math.max((x - e0) / (e1 - e0), 0), 1)
+    return t * t * (3 - 2 * t)
+  },
+  spherical: (radian1: number, radian2: number, radius: number): number[] => {
+    return [
+      Math.cos(radian1) * Math.cos(radian2) * radius,
+      Math.sin(radian1) * radius,
+      Math.cos(radian1) * Math.sin(radian2) * radius,
+    ]
+  },
+  randomArbitrary: (min: number, max: number): number => {
+    return Math.random() * (max - min) + min
+  },
+  randomInt: (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  },
+}
+
 export {
-  sleep
+  sleep,
+  MathEx
 }
--- plugins/utils.ts
+++ plugins/utils.ts
@@ -1,10 +1,21 @@
 import { Plugin } from '@nuxt/types'
-import { sleep } from '@/assets/js/utils'
+import { sleep, MathEx } from '@/assets/js/utils'
 
 declare module 'vue/types/vue' {
   interface Vue {
     $utils: {
       sleep(delay: number): Promise<void>
+      MathEx: {
+        degrees(radian: number): number
+        radians(degree: number): number
+        clamp(value: number, min: number, max: number): number
+        mix(x1: number, x2: number, a: number): number
+        step(e: number, x: number): number
+        smoothstep(e0: number, e1: number, x: number): number | undefined
+        spherical(radian1: number, radian2: number, radius: number): number[]
+        randomArbitrary(min: number, max: number): number
+        randomInt(min: number, max: number): number
+      }
     }
   }
 }
@@ -12,6 +23,7 @@ declare module 'vue/types/vue' {
 const utils: Plugin = (_, inject) => {
   inject('utils', {
     sleep,
+    MathEx,
   })
 }
 
