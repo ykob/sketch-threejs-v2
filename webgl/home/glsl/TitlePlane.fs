b11334a038ae1b3a0797f97998b9dd26e2aebaea
Yoichi Kobayashi <info@tplh.net>
2021-11-05 02:03:21 +0900

    updated the title color.

--- webgl/home/glsl/TitlePlane.fs
+++ webgl/home/glsl/TitlePlane.fs
@@ -1,5 +1,6 @@
 precision highp float;
 
+uniform float time;
 uniform float alphaIndex;
 uniform sampler2D tNoise;
 uniform sampler2D tTitleFill;
@@ -8,8 +9,6 @@ uniform sampler2D tTitleBorder;
 varying vec2 vUv;
 
 void main() {
-  vec2 noiseUv = vUv * vec2(1.0, 0.5);
-
   float alpha1 = texture2D(tTitleFill, vUv).r * (1.0 - step(1.0, alphaIndex));
   float alpha2 = texture2D(tTitleFill, vUv).g * step(1.0, alphaIndex) * (1.0 - step(2.0, alphaIndex));
   float alpha3 = texture2D(tTitleFill, vUv).b * step(2.0, alphaIndex) * (1.0 - step(3.0, alphaIndex));
@@ -18,9 +17,10 @@ void main() {
   float alpha6 = texture2D(tTitleBorder, vUv).b * step(5.0, alphaIndex);
   float alpha = alpha1 + alpha2 + alpha3 + alpha4 + alpha5 + alpha6;
 
-  float noise1 = texture2D(tNoise, noiseUv).r;
+  float noise1 = texture2D(tNoise, vUv * vec2(0.333, 0.166)).r;
+  float noise2 = texture2D(tNoise, vUv * vec2(1.0, 0.5) + vec2(0.0, time * 0.01)).g;
 
-  vec3 color = vec3(0.02, 0.05, 0.12) + noise1;
+  vec3 color = vec3(0.35, 0.45, 0.5) - noise2 * 0.5;
 
   if (alpha < 0.01) discard;
 
