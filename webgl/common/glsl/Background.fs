1b7bc8b11ed20a4bde41be18a118a2a1ce0e6eba
Yoichi Kobayashi <info@tplh.net>
2021-11-18 18:20:17 +0900

    fixed the background animation

--- webgl/common/glsl/Background.fs
+++ webgl/common/glsl/Background.fs
@@ -6,11 +6,7 @@ uniform sampler2D texture;
 varying vec2 vUv;
 
 void main() {
-  vec2 uv = vUv + vec2(
-    sin(vUv.x * 30.0 + time * 0.5) * 0.5 + sin(vUv.x * 60.0 + time) * 0.1,
-    cos(vUv.y * 15.0 + time * 0.5) * 0.5 + cos(vUv.y * 30.0 + time) * 0.1
-  ) * 0.02;
-  vec4 texColor = texture2D(texture, uv);
+  vec4 texColor = texture2D(texture, vUv);
 
   gl_FragColor = texColor - texColor * sin(radians(time * 360.0 * 0.5)) * 0.1;
 }
