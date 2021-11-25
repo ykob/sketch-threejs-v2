f78b1467dc137783dba4679a3595cd1ec6377b40
Yoichi Kobayashi <info@tplh.net>
2021-11-25 23:48:07 +0900

    added the face-man-profile icon.

---
+++ components/icon/FaceManProfile.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+IconBase(
+  iconName = 'face-man-profile'
+  :size = 'size'
+  :fill = 'fill'
+  )
+  path(
+    :d = 'd'
+    )
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mdiFaceManProfile } from '@mdi/js'
+
+export default Vue.extend({
+  props: {
+    size: {
+      type: [Number, String],
+      default: 24,
+    },
+    fill: {
+      type: String,
+      default: 'currentColor',
+    },
+  },
+  computed: {
+    d() {
+      return mdiFaceManProfile
+    },
+  },
+})
+</script>
