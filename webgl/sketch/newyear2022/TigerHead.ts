0761bfe3b8df5a804344de705c2cd2a5ef7cf9fc
Yoichi Kobayashi <info@tplh.net>
2021-12-28 23:00:43 +0900

    clean up.

--- webgl/sketch/newyear2022/TigerHead.ts
+++ webgl/sketch/newyear2022/TigerHead.ts
@@ -17,10 +17,5 @@ export default class TigerHead extends THREE.Mesh {
 
   update(time: number) {
     if (!(this.material instanceof THREE.RawShaderMaterial)) return
-    const { uniforms } = this.material
-
-    uniforms.time.value += time
-    this.rotation.y = uniforms.time.value * 0.1
-    this.rotation.z = uniforms.time.value * 0.05
   }
 }
