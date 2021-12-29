f5cd0bda9213c613ed1c97f412bd3b77dce0408c
Yoichi Kobayashi <info@tplh.net>
2021-12-29 19:14:59 +0900

    adjusted camera position.

--- webgl/sketch/newyear2022/PerspectiveCamera.ts
+++ webgl/sketch/newyear2022/PerspectiveCamera.ts
@@ -10,7 +10,7 @@ export default class PerspectiveCamera extends THREE.PerspectiveCamera {
     super(fov, aspect, near, far)
 
     this.position.z = 7
-    this.position.y = 0.8
+    this.position.y = 0.4
     this.lookAt(0, 1.7, 0)
   }
 
