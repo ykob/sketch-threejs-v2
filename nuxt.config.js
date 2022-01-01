e9b5fd461dffe53f3b75c9d0fab3d79e646f8437
Yoichi Kobayashi <info@tplh.net>
2022-01-02 01:22:33 +0900

    added gtag scripts.

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,17 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: `${ROUTER_BASE}/favicon.ico` },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Homenaje&display=swap' },
     ],
+    script: [
+      { hid: 'gtag', src: `https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}` },
+      {
+        hid: 'gtag', 
+        innerHTML: `window.dataLayer = window.dataLayer || [];
+function gtag(){window.dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.GA_ID}');`
+      }
+    ],
+    __dangerouslyDisableSanitizers: ['script'],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
