701c593b385c19110713f330ff3e1a55f1f97526
Yoichi Kobayashi <info@tplh.net>
2022-01-03 01:51:03 +0900

    set home meta tags.

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
       { hid: 'og:site_name', property: 'og:site_name', content: TITLE },
       { hid: 'apple-mobile-web-app-title', property: 'apple-mobile-web-app-title', content: TITLE },
