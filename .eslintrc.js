e4952d69838c4f62e6ecbd9b7256948602f1e3b8
Yoichi Kobayashi <info@tplh.net>
2022-03-28 11:17:11 +0900

    removed eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,26 +6,9 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
+    'plugin:vue/vue3-recommended',
     'prettier',
   ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {
-    'quotes': ['error', 'single'],
-    'comma-dangle': ['error', {
-      'arrays': 'always-multiline',
-      'objects': 'always-multiline',
-      'imports': 'never',
-      'exports': 'never',
-      'functions': 'never',
-    }],
-    'semi': ['error', 'never', {'beforeStatementContinuationChars': 'never'}],
-    'semi-spacing': ['error', {'after': true, 'before': false}],
-    'semi-style': ['error', 'first'],
-    'no-extra-semi': 'error',
-    'no-unexpected-multiline': 'error',
-    'no-unreachable': 'error',
-  },
+  plugins: ['vue'],
+  rules: {},
 }
