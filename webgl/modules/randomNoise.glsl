ce0c2bcf760722695b77bd791d1666ec0448a479
ykob <info@tplh.net>
2024-12-19 23:47:40 +0900

    clear repo.

--- webgl/modules/randomNoise.glsl
+++
@@ -1,7 +0,0 @@
-#pragma glslify: random = require(@ykob/glsl-util/src/random)
-
-float randomNoise(vec2 p, float time) {
-  return (random(p - vec2(sin(time))) * 2.0 - 1.0);
-}
-
-#pragma glslify: export(randomNoise)
