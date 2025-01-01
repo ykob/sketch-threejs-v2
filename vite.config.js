04c18dfe9af531f43eb2d314fc34bfa5fe604715
ykob <info@tplh.net>
2025-01-01 14:38:11 +0900

    added pageData on vite config for html rendering.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+
+const pageData = {
+  '/index.html': {
+    title: 'Sketch of three.js v2',
+  },
+  '/bubble/index.html': {
+    title: 'Bubble',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(__dirname, 'src'),
+  plugins: [
+    handlebars({
+      context(pagePath) {
+        console.log(pagePath);
+        return pageData[pagePath];
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
+        home: resolve(__dirname, 'src/index.html'),
         bubble: resolve(__dirname, 'src/bubble/index.html'),
       },
     },
