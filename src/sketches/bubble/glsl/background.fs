69f80336c88cf9c53c85e869e2e89962eef0d8eb
ykob <info@tplh.net>
2025-01-13 01:04:59 +0900

    update background color.

--- src/sketches/bubble/glsl/background.fs
+++ src/sketches/bubble/glsl/background.fs
@@ -12,7 +12,7 @@ void main() {
   float noiseR = texture2D(uNoiseTexture, vUv * vec2(2.0, 1.0) + uTime * vec2(0.0, -0.02)).r;
   float noiseG = texture2D(uNoiseTexture, vUv * vec2(2.0, 1.0) + uTime * vec2(-0.02, 0.01)).g;
   float noiseB = texture2D(uNoiseTexture, vUv * vec2(2.0, 1.0) + uTime * vec2(0.02, 0.01)).b;
-  vec3 color = convertHsvToRgb(vec3((noiseR + noiseG + noiseB) * 0.36 + uTime * 0.04, 0.8, 0.28 * vEdge));
+  vec3 color = convertHsvToRgb(vec3((noiseR + noiseG + noiseB) * 0.46 + vUv.x + uTime * 0.04, 0.3, 0.44 * vEdge));
 
   gl_FragColor = vec4(color, 1.0);
 }
