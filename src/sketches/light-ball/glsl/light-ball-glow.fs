533acde889ec49eb702583c9f4a9e5166330b2fa
ykob <info@tplh.net>
2025-01-15 23:26:38 +0900

    update light ball glow color.

--- src/sketches/light-ball/glsl/light-ball-glow.fs
+++ src/sketches/light-ball/glsl/light-ball-glow.fs
@@ -12,16 +12,22 @@ out vec4 fragColor;
 
 void main() {
   vec2 uv = (vUv * 2.0 - vec2(1.0));
-  vec4 noise1 = texture(uNoiseTexture, normalize(uv) + uTime * vec2(0.0, -0.04));
-  vec4 noise2 = texture(uNoiseTexture, normalize(uv) + uTime * vec2(0.04, 0.04));
-  vec4 noise3 = texture(uNoiseTexture, normalize(uv) + uTime * vec2(-0.04, 0.04));
+  vec4 noise1 = texture(uNoiseTexture, normalize(uv) + uTime * vec2(0.0, -0.06));
+  vec4 noise2 = texture(uNoiseTexture, normalize(uv) + uTime * vec2(0.06, 0.06));
+  vec4 noise3 = texture(uNoiseTexture, normalize(uv) + uTime * vec2(-0.06, 0.06));
+  float edge = smoothstep(
+    0.2,
+    0.8,
+    (1.0 - smoothstep(0.4, 0.7, length(uv)))
+      + (noise1.x * noise2.y * noise3.z)) * (1.0 - smoothstep(0.5, 1.0, length(uv))
+  );
   vec3 color = convertHsvToRgb(
     vec3(
-      (noise1.x * noise2.y * noise3.z) * 0.2 + 0.1,
-      (noise1.x * noise2.y * noise3.z) * -4.0 + 1.2,
-      (noise1.x * noise2.y * noise3.z) * 2.0 + 0.4
+      0.1,
+      1.0 - edge * 0.6,
+      0.8 + edge
     )
-  ) + (1.0 - pow(smoothstep(0.45, 0.58, length(uv)), 2.0));
+  );
 
-  fragColor = vec4(color, pow((1.0 - smoothstep(0.5, 1.0, length(uv))), 9.0));
+  fragColor = vec4(color, edge);
 }
