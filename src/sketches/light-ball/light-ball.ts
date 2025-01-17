0facdea2622a04040d1e30452515c8e4767cd5e5
ykob <info@tplh.net>
2025-01-17 13:42:52 +0900

    update light ball animation.

--- src/sketches/light-ball/light-ball.ts
+++ src/sketches/light-ball/light-ball.ts
@@ -5,12 +5,14 @@ import { LightBallGlow } from './light-ball-glow';
 export class LightBall extends Group {
   core: LightBallCore;
   glow: LightBallGlow;
+  time: number;
 
   constructor() {
     super();
 
     this.core = new LightBallCore();
     this.glow = new LightBallGlow();
+    this.time = 0;
   }
   start(texture: Texture) {
     this.core.start(texture);
@@ -21,5 +23,7 @@ export class LightBall extends Group {
   update(delta: number) {
     this.core.update(delta);
     this.glow.update(delta);
+    this.time += delta;
+    this.position.y = Math.sin(this.time) * 0.2;
   }
 }
