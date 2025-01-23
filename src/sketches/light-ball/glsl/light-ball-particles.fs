9b1d9f574df6d9d83fde58e5ca997b68d4b77d37
ykob <info@tplh.net>
2025-01-23 23:35:31 +0900

    clean up shaders.

--- src/sketches/light-ball/glsl/light-ball-particles.fs
+++ src/sketches/light-ball/glsl/light-ball-particles.fs
@@ -11,15 +11,16 @@ out vec4 fragColor;
 
 void main() {
   vec4 noise = texture(uNoiseTexture, vUv + uTime * vec2(0.0, -0.3));
-  vec2 p = gl_PointCoord * 2.0 - 1.0;
-  float r = 1.0 - length(p);
+  vec2 pointCoord = gl_PointCoord * 2.0 - 1.0;
+  float radius = 1.0 - length(pointCoord);
   vec3 color = convertHsvToRgb(
     vec3(
       0.04 + noise.g * 0.3,
-      1.0 - r * 0.5,
-      0.8 + r
+      1.0 - radius * 0.5,
+      0.8 + radius
     )
   );
+  float opacity = (radius - noise.r * 1.4) * 0.24;
 
-  fragColor = vec4(color, (r - noise.r * 1.4) * 0.24);
+  fragColor = vec4(color, opacity);
 }
