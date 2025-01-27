a082b98ed98b7a032580230490cd3ab484c1dcd4
ykob <info@tplh.net>
2025-01-27 21:58:54 +0900

    added updated on sketch meta data.

--- src/consts/page-data.ts
+++ src/consts/page-data.ts
@@ -9,11 +9,13 @@ export const pageData = [
     path: '/sketches/bubble/index.html',
     title: 'Bubble',
     description: 'render the bubble balls with noize texture.',
+    updated: '2025-01-10T10:00:00+09:00',
   },
   {
     key: 'light-ball',
     path: '/sketches/light-ball/index.html',
     title: 'Light Ball',
     description: 'render the light ball with noize texture.',
+    updated: '2025-01-24T10:00:00+09:00',
   },
 ];
