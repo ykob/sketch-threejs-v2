c54d55b3594e80242c8195a63787d25b743b149c
ykob <info@tplh.net>
2025-02-14 23:57:50 +0900

    update description.

--- src/consts/meta-data.ts
+++ src/consts/meta-data.ts
@@ -1,5 +1,5 @@
 export const metaData = {
   title: 'Sketch of three.js v2',
-  description: 'Sketch of three.js v2',
+  description: 'Interactive sketches made with three.js.',
   keywords: 'three.js, TypeScirpt, Vite',
 };
