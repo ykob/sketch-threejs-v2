ad136d3cfa335dfbe89478d4c1a7c46b532b1286
ykob <info@tplh.net>
2025-02-21 18:06:26 +0900

    update the way to set instancing params for confetti.

--- src/sketches/confetti/confetti.ts
+++ src/sketches/confetti/confetti.ts
@@ -21,32 +21,16 @@ export class Confetti extends InstancedMesh<
 > {
   params: {
     position: Vector3;
-    positionBase: Vector3;
     speed: number;
     euler: Euler;
     eulerSpeed: Euler;
     scale: Vector3;
-  }[] = Array.from({ length: count }, () => ({
-    position: new Vector3(),
-    positionBase: new Vector3(
-      (Math.random() - 0.5) * 20,
-      Math.random() * 20 - 10,
-      (Math.random() - 0.5) * 20,
-    ),
-    speed: Math.random() * 2 + 1,
-    euler: new Euler(
-      Math.random() * Math.PI,
-      Math.random() * Math.PI,
-      Math.random() * Math.PI,
-    ),
-    eulerSpeed: new Euler(1, 1, 1),
-    scale: new Vector3(1, 1, 1),
-  }));
+  }[] = [];
   matrix: Matrix4 = new Matrix4();
   quaternion: Quaternion = new Quaternion();
 
   constructor() {
-    const baseGeometry = new PlaneGeometry(0.1, 0.1);
+    const baseGeometry = new PlaneGeometry(0.25, 0.25);
     const geometry = new InstancedBufferGeometry();
 
     geometry.setAttribute('position', baseGeometry.attributes.position);
@@ -70,13 +54,31 @@ export class Confetti extends InstancedMesh<
       count,
     );
   }
-  start() {}
+  start() {
+    const scale = Math.random() * 0.5 + 0.5;
+    this.params = Array.from({ length: count }, () => ({
+      position: new Vector3(
+        (Math.random() * 2 - 1) * 5,
+        (Math.random() * 2 - 1) * 5,
+        (Math.random() * 2 - 1) * 20,
+      ),
+      speed: Math.random() * 2 + 1,
+      euler: new Euler(
+        Math.random() * Math.PI,
+        Math.random() * Math.PI,
+        Math.random() * Math.PI,
+      ),
+      eulerSpeed: new Euler(
+        (Math.random() * 0.4 + 0.1) * Math.PI,
+        (Math.random() * 0.4 + 0.1) * Math.PI,
+        (Math.random() * 0.4 + 0.1) * Math.PI,
+      ),
+      scale: new Vector3(scale, scale, scale),
+    }));
+  }
   update(delta: number) {
     for (let i = 0; i < this.params.length; i++) {
-      const { position, positionBase, speed, euler, eulerSpeed, scale } =
-        this.params[i];
-      position.x = positionBase.x;
-      position.y = positionBase.y;
+      const { position, speed, euler, eulerSpeed, scale } = this.params[i];
       position.z = ((position.z + delta * speed + 20) % 40) - 20;
       euler.x += delta * eulerSpeed.x;
       euler.y += delta * eulerSpeed.y;
