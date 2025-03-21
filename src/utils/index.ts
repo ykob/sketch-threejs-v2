1e56ec28e1d45d8a30df7bb9f14395cd5575ecb1
ykob <info@tplh.net>
2025-03-21 12:01:38 +0900

    update utils entry points.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './debounce';
 export * from './easings';
 export * from './get-coord-as-pixel';
+export * from './hook3';
 export * from './radians';
 export * from './spherical';
 export * from './toggle-sketch-ui';
