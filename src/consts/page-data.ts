265f76015580f51a12d9950c091d724813acb912
ykob <info@tplh.net>
2025-04-05 23:04:52 +0900

    fixed updated value.

--- src/consts/page-data.ts
+++ src/consts/page-data.ts
@@ -10,7 +10,7 @@ export const pageData = [
     path: '/sketches/drag/index.html',
     title: 'Drag',
     description: 'Emit particles following mouse or touch events.',
-    updated: '2025-03-15T10:00:00+09:00',
+    updated: '2025-03-15',
   },
   {
     key: 'confetti',
@@ -18,27 +18,27 @@ export const pageData = [
     title: 'Confetti',
     description:
       'Simple geometry instancing using a single planar object and a single texture.',
-    updated: '2025-02-23T10:00:00+09:00',
+    updated: '2025-02-23',
   },
   {
     key: 'img',
     path: '/sketches/img/index.html',
     title: 'Img',
     description: 'Make a WebGL plane object look like an HTML img element.',
-    updated: '2025-02-12T10:00:00+09:00',
+    updated: '2025-02-12',
   },
   {
     key: 'light-ball',
     path: '/sketches/light-ball/index.html',
     title: 'Light Ball',
     description: 'render the light ball with noize texture.',
-    updated: '2025-01-24T10:00:00+09:00',
+    updated: '2025-01-24',
   },
   {
     key: 'bubble',
     path: '/sketches/bubble/index.html',
     title: 'Bubble',
     description: 'render the bubble balls with noize texture.',
-    updated: '2025-01-10T10:00:00+09:00',
+    updated: '2025-01-10',
   },
 ];
