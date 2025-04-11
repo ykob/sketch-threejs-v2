f00f66849f00200398315ac92af6685b323cac2b
ykob <info@tplh.net>
2025-04-11 22:17:50 +0900

    update glitch effect.

--- src/sketches/glitch-img/glsl/image.fs
+++ src/sketches/glitch-img/glsl/image.fs
@@ -15,20 +15,31 @@ float whiteNoise(vec2 uv) {
 }
 
 void main() {
+  float time = floor(uTime * 20.0) / 20.0;
   float noisePower = smoothstep(
-    0.8,
+    0.7,
     1.0,
-    sin(uTime) * 0.5 + 0.5
+    sin(time) * 0.5 + 0.5
   );
-  vec2 noiseUv = vec2(vUv.x, vUv.y * 9.0 / 16.0 + (1.0 - 9.0 / 16.0) / 2.0) + vec2(uTime * 100.0, 0.0);
+  vec2 noiseUv = vec2(
+    vUv.x + time * 100.0,
+    vUv.y * 9.0 / 16.0 + (1.0 - 9.0 / 16.0) / 2.0
+  );
   float glitch = whiteNoise(
     floor(
-      noiseUv * vec2(0.05 + whiteNoise(vec2(uTime)) * 2.0, 1.0 - whiteNoise(vec2(uTime * 5.0)) * 10.0) * 10.0
+      noiseUv * vec2(
+        0.5 + whiteNoise(vec2(time)) * 1.5,
+        1.0 - whiteNoise(vec2(time * 5.0)) * 8.0
+      ) * 10.0
     ) / 10.0
   );
-  float glitchMask = step(glitch, 0.005 + noisePower * 0.1);
+  float glitchMask = step(glitch, 0.005 + noisePower * 0.04);
   vec3 color = texture(uImageTexture, vUv).rgb;
   vec3 glitchColor = texture(uImageTexture, vUv + noisePower + 0.1).rgb;
 
-  fragColor = vec4(color * (1.0 - glitchMask) + (glitchColor) * glitchMask, 1.0);
+  fragColor = vec4(
+    color * (1.0 - glitchMask)
+      + (glitchColor) * glitchMask
+      - (whiteNoise(vUv + time) * 2.0 - 1.0) * 0.1,
+  1.0);
 }
