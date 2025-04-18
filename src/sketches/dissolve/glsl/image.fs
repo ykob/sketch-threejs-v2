a985f78ecb4ee923d59c8362df69dee6e6f42493
ykob <info@tplh.net>
2025-04-18 17:35:59 +0900

    added dissolve hide mask and hide effect.

--- src/sketches/dissolve/glsl/image.fs
+++ src/sketches/dissolve/glsl/image.fs
@@ -13,9 +13,14 @@ out vec4 fragColor;
 void main() {
   vec2 noiseUv = vec2(vUv.x, vUv.y * 9.0 / 16.0 + (1.0 - 9.0 / 16.0) / 2.0);
 
-  float dissolveMask = 1.0 - smoothstep(
+  float dissolveMaskShow = 1.0 - smoothstep(
     -1.0 + uTime,
     0.0 + uTime,
+    length(noiseUv * 2.0 - 1.0)
+    );
+  float dissolveMaskHide = 1.0 - smoothstep(
+    -2.0 + uTime,
+    -1.0 + uTime,
     length(noiseUv * 2.0 - 1.0)
     );
   float dissolveNoise1 = texture(uNoiseTexture, noiseUv + vec2(uTime * 0.04, 0.0)).r;
@@ -23,7 +28,7 @@ void main() {
   float dissolve = smoothstep(
     0.0,
     1.0,
-    ((dissolveNoise1 + dissolveNoise2) - 1.0) * 0.8 + dissolveMask * 1.8
+    ((dissolveNoise1 + dissolveNoise2) - 1.0) * 0.8 + dissolveMaskShow * (1.0 - dissolveMaskHide) * 1.8
     );
   
   float glowNoise1 = texture(uNoiseTexture, noiseUv * 0.5 + vec2(uTime * 0.04, 0.0)).r;
